c6a21c73440fb6adbd26b04455a8e4b428861d62
pipo-omaka <pherawichtae@gmail.com>
2025-02-06 12:23:42 +0700

    2

---
+++ EmployeeCard.java
@@ -0,0 +1,9 @@
+public class EmployeeCard extends Card{
+
+    public EmployeeCard(String username, String password, String status){
+        super(username, password, status);
+    }
+
+    public static class VisitorCard {
+    }
+}
--- Main.java
+++ Main.java
@@ -4,31 +4,33 @@ public class Main {
     public static void main(String[] args) {
         AccessControlSystem system = new AccessControlSystem();
 
-//        // Create a new access card
-//        TimeBasedAccessCard card1 = new TimeBasedAccessCard("CARD123", LocalDateTime.now().plusDays(30));
-//        card1.addAccessLevel("Low Floor");
-//
-//        // Register the card in the system
-//        system.registerCard(card1);
-//
-//        // Modify permissions
-//        system.modifyCard("CARD123", "ADD", "High Floor");
-//
-//        // Validate access
-//        System.out.println("Access Granted? " + system.validateAccess("CARD123", "High Floor"));
-//
-//        // Show audit logs
-//        system.showAuditLogs();
+        // Create a new access card
+        TimeBasedAccessCard card1 = new TimeBasedAccessCard("CARD123", LocalDateTime.now().plusDays(30));
+        card1.addAccessLevel("Low Floor");
 
-        Card a = new Card("a","123", "Manager");
+        // Register the card in the system
+        system.registerCard(card1);
 
-        String result = a.getPrivateCode("a", "123");
-        System.out.println(result);
+        // Modify permissions
+        system.modifyCard("CARD123", "REVOKE", "Low Floor");
+
+        // Validate access
+        System.out.println("Access Granted? " + system.validateAccess("CARD123", "Medium Floor"));
+
+        // Show audit logs
+        system.showAuditLogs();
 
 
-        a.setStatus("Employee");
-        String sum = a.getStatus();
-        System.out.println(sum);
+//        //add && edit
+//        Card a = new Card("a","123", "Manager");
+//
+//        System.out.println(a.getPrivateCode("a", "123"));
+//
+//        System.out.println(a.getStatus());
+//
+//        a.setStatus("Employee");
+//
+//        System.out.println(a.getStatus());
 
     }
 }
